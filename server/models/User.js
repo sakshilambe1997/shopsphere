8a4b2dce08fb1bb0c1ede97f28278d3d366ae21b
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-12-13 14:49:15 +0530

    user model created

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+import {Schema,model} from "mongoose"
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    phone:{
+        type:String,
+        unique:true
+    },
+    address:{
+        type:String
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+    password:{
+        type: String,
+        required:true
+    }
+},{
+    timestamps:true
+})
