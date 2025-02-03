173bcec3a2b72baba8d7b9b096f193e0bcf35cad
sakshilambe1997 <sakshilambe1997@gmail.com>
2025-02-03 16:31:18 +0530

    routing added

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import Signup from './views/Signup/Signup';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+   {
+    path:"/",
+    element:<Home/>
+   },
+
+   {
+    path:"/login",
+    element:<Login/>
+   },
+
+   {
+    path:"/signup",
+    element:<Signup/>
+   },
+])
+
 root.render(
-  <React.StrictMode>
-   <h1>Hello</h1>
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
